b4c0bc1efee5204a4d5db38208c950b73a5b8854
weeeen <wen980602@gmail.com>
2026-04-22 16:26:36 +0800

    feat(main): 新增環境變數載入與 API Key 測試代碼

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+load_dotenv()
+print(os.getenv("OPENAI_API_KEY"))
+
 def main():
     ab="apple"
     print(f"歡迎使用{ab}")
+
+load_dotenv()
+print(os.getenv("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
