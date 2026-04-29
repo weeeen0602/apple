6918a5795d29fcc8a0b66a5b48c864653e5077fe
weeeen <wen980602@gmail.com>
2026-04-29 16:20:53 +0800

    feat(main): 實作 LLM 初始化邏輯，並更新模型至 gemma4:26b

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from langchain_openai import ChatOpenAI
 load_dotenv()
 
 
+
 def main():
     api_key=os.getenv("OPENAI_API_KEY")
     print(api_key)
     ab="apple"
     print(f"歡迎使用{ab}")
-    if  api_key:
-        print("API key is  set")
+    if api_key:
+        print("API key is set")
+        llm = ChatOpenAI(
+            model="gemma4:26b",
+            base_url="http://203.71.78.31:8000/v1",
+            api_key=api_key,
+            temperature=0.0,
+        )
+        print("LLM 已成功初始化")
     else:
         print("API key is not set")
         return 
