89a95671c8de3222caed5302c43191479ea09f97
weeeen <wen980602@gmail.com>
2026-04-29 14:21:53 +0800

    refactor(main): 整理環境變數讀取，將其移入 main 函數內

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
 
 def main():
+    api_key=os.getenv("OPENAI_API_KEY")
+    print(api_key)
     ab="apple"
     print(f"歡迎使用{ab}")
-
-load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
