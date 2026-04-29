3dbafef30a4d59e2bbcdf84bd5c30fca24212596
weeeen <wen980602@gmail.com>
2026-04-29 14:56:10 +0800

    feat(main): 完善 API Key 檢查與狀態顯示邏輯

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 load_dotenv()
 
+
 def main():
     api_key=os.getenv("OPENAI_API_KEY")
     print(api_key)
     ab="apple"
     print(f"歡迎使用{ab}")
+    if  api_key:
+        print("API key is  set")
+    else:
+        print("API key is not set")
 
+    
 
 if __name__ == "__main__":
     main()
